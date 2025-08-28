55d1dcd62a2660bc663f1df2fda46580b9f3ec07
hehe <hehe>
2025-08-28 09:09:54 +0800

    Initial Commit

---
+++ Fabrica_App/Fabrica_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fabrica_AppApp.swift
+//  Fabrica_App
+//
+//  Created by STUDENT on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Fabrica_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fabrica_AppTests/Fabrica_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fabrica_AppTests.swift
+//  Fabrica_AppTests
+//
+//  Created by STUDENT on 8/28/25.
+//
+
+import Testing
+@testable import Fabrica_App
+
+struct Fabrica_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
